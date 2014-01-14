d0e11e6ff793940bae96089b2c8f7bf9897cd512
Stuart Knightley <noreply>
2014-01-14 11:11:34 -0800

    Add component tree

---
+++ ui/tree.reel/tree.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/tree.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tree
+ * @extends Component
+ */
+exports.Tree = Component.specialize(/** @lends Tree# */ {
+    constructor: {
+        value: function Tree() {
+            this.super();
+        }
+    }
+});
