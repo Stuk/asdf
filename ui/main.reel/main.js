67d44c9c174480f6b690e9c2ba0389b6d97c1bec
Stuart Knightley <noreply>
2014-02-26 01:48:30 +0000

    Update component /ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,5 +13,11 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
+    },
+    
+    handleAction: {
+        value: function (event) {
+            console.log("action", event);
+        }
     }
 });
