fd0d7172367fc7d4a8acb0da9917bb41d7232400
Stuart Knightley <noreply>
2014-04-02 23:49:27 +0000

    Add component name-list

---
+++ ui/name-list.reel/name-list.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/name-list.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class NameList
+ * @extends Component
+ */
+exports.NameList = Component.specialize(/** @lends NameList# */ {
+    constructor: {
+        value: function NameList() {
+            this.super();
+        }
+    }
+});
