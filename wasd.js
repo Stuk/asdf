c475170f03bebf6a3da80fb4770795c1f3594c79
Stuart Knightley <noreply>
2014-04-08 23:27:38 +0000

    Add module wasd

---
+++ wasd.js
@@ -0,0 +1,16 @@
+/**
+ * @module wasd
+ * @requires montage/core/core
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Wasd
+ * @extends Montage
+ */
+exports.Wasd = Montage.specialize(/** @lends Wasd# */ {
+    constructor: {
+        value: function Wasd() {
+            this.super();
+        }
+    }
+});
