14a1e5390e9f02aaed6c02c5b800b3b5ba5d9588
Stuart Knightley <noreply>
2014-04-09 00:10:08 +0000

    Update component /wasd.js

--- wasd.js
+++ wasd.js
@@ -1,12 +1,5 @@
-/**
- * @module wasd
- * @requires montage/core/core
- */
 var Montage = require("montage/core/core").Montage;
-/**
- * @class Wasd
- * @extends Montage
- */
+
 exports.Wasd = Montage.specialize(/** @lends Wasd# */ {
     constructor: {
         value: function Wasd() {
